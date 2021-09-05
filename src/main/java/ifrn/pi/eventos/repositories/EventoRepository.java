b920b814f7a8347b98f5d36e9433716baf6e9b41
ohkerou <carolbraga0866@gmail.com>
2021-09-05 20:52:22 -0300

    Criando repository para Evento

---
+++ src/main/java/ifrn/pi/eventos/repositories/EventoRepository.java
@@ -0,0 +1,10 @@
+package ifrn.pi.eventos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ifrn.pi.eventos.models.Evento;
+
+public interface EventoRepository extends JpaRepository<Evento, Long> {
+
+	
+}
