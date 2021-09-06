508fa7576151e9503a1c23f25e0245bf82ed441c
ohkerou <carolbraga0866@gmail.com>
2021-09-05 22:10:46 -0300

    Criando a listagem de eventos

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,21 +1,26 @@
 package ifrn.pi.eventos.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import ifrn.pi.eventos.models.Evento;
 import ifrn.pi.eventos.repositories.EventoRepository;
 
 
 @Controller
+@RequestMapping("/eventos")
 public class EventosController {
 	
 	@Autowired
 	private EventoRepository er;
 	
-	@RequestMapping("/eventos/fom")
+	@GetMapping("/eventos/fom")
 	public String form() {
 		return "eventos/formEvento";
 	}
@@ -28,6 +33,14 @@ public class EventosController {
 	
 		return "eventos/evento-adicionado";
 		
+	}
+	@GetMapping("/eventos")
+	public ModelAndView listar() {
+		
+		List<Evento> eventos = er.findAll();
+		ModelAndView mv = new ModelAndView("eventos/lista");
+		mv.addObject("eventos", eventos);
+		return mv;
 	}
 }
 
--- src/main/java/ifrn/pi/eventos/controllers/indexController.java
+++ src/main/java/ifrn/pi/eventos/controllers/indexController.java
@@ -8,7 +8,7 @@ public class indexController {
 	@RequestMapping("/")
 	public String index() {
 			System.out.println("Chamou o método index");
-		return "home";
+		return "redirect:/eventos";
 	}
 	
 }
