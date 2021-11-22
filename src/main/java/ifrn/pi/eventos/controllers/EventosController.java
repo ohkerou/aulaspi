4a6c4dad2ea1117a10c4864acbf9da0c6ac5e388
ohkerou <carolbraga0866@gmail.com>
2021-11-22 19:44:28 -0300

    atualizando

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,61 +1,90 @@
 package ifrn.pi.eventos.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import ifrn.pi.eventos.models.Evento;
 import ifrn.pi.eventos.repositories.EventoRepository;
 
-import org.springframework.web.bind.annotation.PostMapping;
-
-
 @Controller
+@RequestMapping("/eventos")
 public class EventosController {
 	
 	@Autowired
 	private EventoRepository er;
-	
-	@RequestMapping("/eventos/form")
+
+	@GetMapping("/form")
 	public String form() {
-		return "formEvento";
-
+		return "eventos/formEvento";
 	}
-	/*@PostMapping ("/eventos")
-	public String novo() {
-		return "novoo";
 
-	}*/
-	/*@PostMapping ("/eventos")
-	public String novoo(String nome, String local, String data, String horario){
-		System.out.println(nome);
-		System.out.println(local);
-		System.out.println(data);
-		System.out.println(horario);
-		return "novoo";
-		
-	}
-	
-	
-	@PostMapping ("/eventos")
-	public String novoo(Evento evento){
-		 
-		System.out.println(evento.getNome());
-		System.out.println(evento.getLocal());
-		System.out.println(evento.getData());
-		System.out.println(evento.getHorario());
-		
-		return "novoo";
-		
-	}*/
-	
-	@PostMapping("/eventos")
+	@PostMapping
 	public String adicionar(Evento evento) {
-		
 		System.out.println(evento);
 		er.save(evento);
-		return "evento-adicionado";
+
+		return "eventos/evento-adicionado";
+
 	}
 	
+	@GetMapping
+	public ModelAndView listar() {
+		List<Evento> eventos = er.findAll();
+		ModelAndView mv = new ModelAndView("eventos/lista");
+		mv.addObject("eventos", eventos);
+		return mv;
+	}
+	
+	@GetMapping("/{id}")
+	public ModelAndView detalhar(@PathVariable Long id) {
+		 ModelAndView md = new  ModelAndView();
+		 Optional<Evento> opt = er.findById(id);
+		 
+		 if(opt.isEmpty()) {
+			 md.setViewName("redirect:/eventos");
+			 return md;
+		 }
+		 
+		 md.setViewName("eventos/detalhes");
+		 Evento evento = opt.get();
+		 md.addObject("evento", evento);
+		 
+		 return md;
+	}	
+
 }
+	
 
+/*
+ * @PostMapping("/eventos") public String novo() { return "enovo";
+ * 
+ * }
+ */
 
+/*
+ * @PostMapping("/eventos") public String enovo(String nome, String local,
+ * String data, String horario) { System.out.println(nome);
+ * System.out.println(local); System.out.println(data);
+ * System.out.println(horario); return "enovo";
+ * 
+ * }
+ */
+
+/*
+ * @PostMapping("/eventos") public String enovo(Evento evento) {
+ * System.out.println(evento.getNome()); System.out.println(evento.getLocal());
+ * System.out.println(evento.getData());
+ * System.out.println(evento.getHorario());
+ * 
+ * return "enovo";
+ * 
+ * }
+ */
--- src/main/java/ifrn/pi/eventos/controllers/indexController.java
+++ src/main/java/ifrn/pi/eventos/controllers/indexController.java
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+
 public class indexController {
-	@RequestMapping("/")
-	public String index() {
-			System.out.println("Chamou o método index");
-		return "home";
-	}
 	
+	@RequestMapping("/")
+	public String index(){
+		System.out.println("Chamou o método index");
+		return "redirect:/eventos";
+	}
+		
 }
